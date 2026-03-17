5fb2d2bce173a262ad1c6a549b08dba84b439942
NewmanB1 <179244577+NewmanB1@users.noreply.github.com>
2026-03-17 19:44:19 -0400

    Update basin_search.py

--- engine/basin_search.py
+++ engine/basin_search.py
@@ -1,1 +1,14 @@
+from collections import defaultdict
+from .card_parser import extract_mechanics
 
+def build_mechanic_index(cards):
+
+    index = defaultdict(list)
+
+    for card in cards:
+        mechanics = extract_mechanics(card["text"])
+
+        for m in mechanics:
+            index[m].append(card["name"])
+
+    return index
