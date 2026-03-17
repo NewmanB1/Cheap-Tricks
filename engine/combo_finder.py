1118b3ccc3fb9da68bc7560c63edd44652c14f6d
NewmanB1 <179244577+NewmanB1@users.noreply.github.com>
2026-03-17 19:45:00 -0400

    Update combo_finder.py

--- engine/combo_finder.py
+++ engine/combo_finder.py
@@ -1,1 +1,11 @@
+import networkx as nx
 
+def find_loops(graph):
+
+    combos = []
+
+    for cycle in nx.simple_cycles(graph.to_directed()):
+        if len(cycle) <= 4:
+            combos.append(cycle)
+
+    return combos
